9eaf2a22f2e5e14b0e4155db878f19ae4ad25a9f
Eric Herman <eric@freesa.org>
2016-04-02 11:03:30 +0200

    also print the function name in which the check failed, (with file:line)

--- echeck.c
+++ echeck.c
@@ -1,95 +1,101 @@
 #include "echeck.h"
 #include <string.h>
 
-int echeck_char_m(FILE *err, const char *file, int line, char actual,
-		  char expected, const char *msg)
+int echeck_char_m(FILE *err, const char *func, const char *file, int line,
+		  char actual, char expected, const char *msg)
 {
 	if (expected == actual) {
 		return 0;
 	}
-	fprintf(err,
-		"FAIL: %s%sExpected '%c' but was '%c' [File: %s Line: %d]\n",
+	fprintf(err, "FAIL: %s%sExpected '%c' but was '%c' [%s%s%s:%d]\n",
 		(msg == NULL) ? "" : msg, (msg == NULL) ? "" : " ", expected,
-		actual, file, line);
+		actual, (func == NULL) ? "" : func,
+		(func == NULL) ? "" : " at ", file, line);
 	return 1;
 }
 
-int echeck_unsigned_long_m(FILE *err, const char *file, int line,
-			   unsigned long actual, unsigned long expected,
-			   const char *msg)
+int echeck_unsigned_long_m(FILE *err, const char *func, const char *file,
+			   int line, unsigned long actual,
+			   unsigned long expected, const char *msg)
 {
 	if (expected == actual) {
 		return 0;
 	}
-	fprintf(err, "FAIL: %s%sExpected %lu but was %lu [File: %s Line: %d]\n",
+	fprintf(err, "FAIL: %s%sExpected %lu but was %lu [%s%s%s:%d]\n",
 		(msg == NULL) ? "" : msg, (msg == NULL) ? "" : " ", expected,
-		actual, file, line);
+		actual, (func == NULL) ? "" : func,
+		(func == NULL) ? "" : " at ", file, line);
 	return 1;
 }
 
-int echeck_unsigned_int_m(FILE *err, const char *file, int line,
-			  unsigned int actual, unsigned int expected,
+int echeck_unsigned_int_m(FILE *err, const char *func, const char *file,
+			  int line, unsigned int actual, unsigned int expected,
 			  const char *msg)
 {
 	if (expected == actual) {
 		return 0;
 	}
-	fprintf(err, "FAIL: %s%sExpected %u but was %u [File: %s Line: %d]\n",
+	fprintf(err, "FAIL: %s%sExpected %u but was %u [%s%s%s:%d]\n",
 		(msg == NULL) ? "" : msg, (msg == NULL) ? "" : " ", expected,
-		actual, file, line);
+		actual, (func == NULL) ? "" : func,
+		(func == NULL) ? "" : " at ", file, line);
 	return 1;
 }
 
-int echeck_int_m(FILE *err, const char *file, int line, int actual,
-		 int expected, const char *msg)
+int echeck_int_m(FILE *err, const char *func, const char *file, int line,
+		 int actual, int expected, const char *msg)
 {
 	if (expected == actual) {
 		return 0;
 	}
-	fprintf(err, "FAIL: %s%sExpected %d but was %d [File: %s Line: %d]\n",
+	fprintf(err, "FAIL: %s%sExpected %d but was %d [%s%s%s:%d]\n",
 		(msg == NULL) ? "" : msg, (msg == NULL) ? "" : " ", expected,
-		actual, file, line);
+		actual, (func == NULL) ? "" : func,
+		(func == NULL) ? "" : " at ", file, line);
 	return 1;
 }
 
-int echeck_long_m(FILE *err, const char *file, int line, long actual,
-		  long expected, const char *msg)
+int echeck_long_m(FILE *err, const char *func, const char *file, int line,
+		  long actual, long expected, const char *msg)
 {
 	if (expected == actual) {
 		return 0;
 	}
-	fprintf(err, "FAIL: %s%sExpected %ld but was %ld [File: %s Line: %d]\n",
+	fprintf(err, "FAIL: %s%sExpected %ld but was %ld [%s%s%s:%d]\n",
 		(msg == NULL) ? "" : msg, (msg == NULL) ? "" : " ", expected,
-		actual, file, line);
+		actual, (func == NULL) ? "" : func,
+		(func == NULL) ? "" : " at ", file, line);
 	return 1;
 }
 
-int echeck_size_t_m(FILE *err, const char *file, int line, size_t actual,
-		    size_t expected, const char *msg)
+int echeck_size_t_m(FILE *err, const char *func, const char *file, int line,
+		    size_t actual, size_t expected, const char *msg)
 {
 	if (expected == actual) {
 		return 0;
 	}
-	fprintf(err, "FAIL: %s%sExpected %lu but was %lu [File: %s Line: %d]\n",
+	fprintf(err, "FAIL: %s%sExpected %lu but was %lu [%s%s%s:%d]\n",
 		(msg == NULL) ? "" : msg, (msg == NULL) ? "" : " ",
-		(unsigned long)expected, (unsigned long)actual, file, line);
+		(unsigned long)expected, (unsigned long)actual,
+		(func == NULL) ? "" : func, (func == NULL) ? "" : " at ", file,
+		line);
 	return 1;
 }
 
-int echeck_str_m(FILE *err, const char *file, int line, const char *actual,
-		 const char *expected, const char *msg)
+int echeck_str_m(FILE *err, const char *func, const char *file, int line,
+		 const char *actual, const char *expected, const char *msg)
 {
 	if (strcmp(expected, actual) == 0) {
 		return 0;
 	}
-	fprintf(err,
-		"FAIL: %s%sExpected '%s' but was '%s' [File: %s Line: %d]\n",
+	fprintf(err, "FAIL: %s%sExpected '%s' but was '%s' [%s%s%s:%d]\n",
 		(msg == NULL) ? "" : msg, (msg == NULL) ? "" : " ", expected,
-		actual, file, line);
+		actual, (func == NULL) ? "" : func,
+		(func == NULL) ? "" : " at ", file, line);
 	return 1;
 }
 
-int echeck_byte_array_m(FILE *err, const char *file, int line,
+int echeck_byte_array_m(FILE *err, const char *func, const char *file, int line,
 			unsigned char *actual, size_t actual_len,
 			unsigned char *expected, size_t expected_len,
 			const char *msg)
@@ -98,10 +104,12 @@ int echeck_byte_array_m(FILE *err, const char *file, int line,
 	size_t i;
 	const char *fmt;
 
-	fmt = "actual/expected length mis-match %d != %d [File: %s Line: %d]\n";
+	fmt = "actual/expected length mis-match %d != %d [%s%s%s:%d]\n";
 
 	if (actual_len != expected_len) {
-		fprintf(err, fmt, actual_len, expected_len, file, line);
+		fprintf(err, fmt, actual_len, expected_len,
+			(func == NULL) ? "" : func,
+			(func == NULL) ? "" : " at ", file, line);
 		goto fail;
 	}
 	for (i = 0; i < actual_len; i++) {
@@ -114,8 +122,9 @@ int echeck_byte_array_m(FILE *err, const char *file, int line,
 	return 0;
 
 fail:
-	fprintf(err, "FAIL: %s%s[File: %s Line: %d]\n",
-		(msg == NULL) ? "" : msg, (msg == NULL) ? "" : " ", file, line);
+	fprintf(err, "FAIL: %s%s[%s%s%s:%d]\n", (msg == NULL) ? "" : msg,
+		(msg == NULL) ? "" : " ", (func == NULL) ? "" : func,
+		(func == NULL) ? "" : " at ", file, line);
 	for (i = 0; i < actual_len; i++) {
 		fprintf(err, "actual[%lu]=%02x\n", (unsigned long)i, actual[i]);
 	}
@@ -126,15 +135,15 @@ fail:
 	return 1;
 }
 
-int echeck_ptr_m(FILE *err, const char *file, int line, const void *actual,
-		 const void *expected, const char *msg)
+int echeck_ptr_m(FILE *err, const char *func, const char *file, int line,
+		 const void *actual, const void *expected, const char *msg)
 {
 	if (expected == actual) {
 		return 0;
 	}
-	fprintf(err,
-		"FAIL: %s%sExpected '%p' but was '%p' [File: %s Line: %d]\n",
+	fprintf(err, "FAIL: %s%sExpected '%p' but was '%p' [%s%s%s:%d]\n",
 		(msg == NULL) ? "" : msg, (msg == NULL) ? "" : " ", expected,
-		actual, file, line);
+		actual, (func == NULL) ? "" : func,
+		(func == NULL) ? "" : " at ", file, line);
 	return 1;
 }
--- echeck.h
+++ echeck.h
@@ -5,162 +5,199 @@
 #include <stdlib.h>		/* for size_t */
 #include <stdio.h>		/* for FILE */
 
+#ifdef __GNUC__
+#define ECHECK_FUNC __PRETTY_FUNCTION__
+#else
+#if (__STDC_VERSION__ >= 199901L)
+#define ECHECK_FUNC __func__
+#else
+#define ECHECK_FUNC NULL
+#endif /* __GNUC__ */
+#endif /* __STDC_VERSION__ */
+
 /*check char*/
-int echeck_char_m(FILE *err, const char *file, int line, char actual,
-		  char expected, const char *msg);
+int echeck_char_m(FILE *err, const char *func, const char *file, int line,
+		  char actual, char expected, const char *msg);
 
 #define fcheck_char_m(log, actual, expected, msg)\
-	echeck_char_m(log, __FILE__, __LINE__, actual, expected, msg)
+	echeck_char_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define check_char_m(actual, expected, msg)\
-	echeck_char_m(stderr, __FILE__, __LINE__, actual, expected, msg)
+	echeck_char_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define fcheck_char(log, actual, expected)\
-	echeck_char_m(log, __FILE__, __LINE__, actual, expected, NULL)
+	echeck_char_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 #define check_char(actual, expected)\
-	echeck_char_m(stderr, __FILE__, __LINE__, actual, expected, NULL)
+	echeck_char_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 /* check int */
-int echeck_int_m(FILE *err, const char *file, int line, int actual,
-		 int expected, const char *msg);
+int echeck_int_m(FILE *err, const char *func, const char *file, int line,
+		 int actual, int expected, const char *msg);
 
 #define fcheck_int_m(log, actual, expected, msg)\
-	echeck_int_m(log, __FILE__, __LINE__, actual, expected, msg)
+	echeck_int_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define check_int_m(actual, expected, msg)\
-	echeck_int_m(stderr, __FILE__, __LINE__, actual, expected, msg)
+	echeck_int_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define fcheck_int(log, actual, expected)\
-	echeck_int_m(log, __FILE__, __LINE__, actual, expected, NULL)
+	echeck_int_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 #define check_int(actual, expected)\
-	echeck_int_m(stderr, __FILE__, __LINE__, actual, expected, NULL)
+	echeck_int_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 /* check long */
-int echeck_long_m(FILE *err, const char *file, int line, long actual,
-		  long expected, const char *msg);
+int echeck_long_m(FILE *err, const char *func, const char *file, int line,
+		  long actual, long expected, const char *msg);
 
 #define fcheck_long_m(log, actual, expected, msg)\
-	echeck_long_m(log, __FILE__, __LINE__, actual, expected, msg)
+	echeck_long_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define check_long_m(actual, expected, msg)\
-	echeck_long_m(stderr, __FILE__, __LINE__, actual, expected, msg)
+	echeck_long_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define fcheck_long(log, actual, expected)\
-	echeck_long_m(log, __FILE__, __LINE__, actual, expected, NULL)
+	echeck_long_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 #define check_long(actual, expected)\
-	echeck_long_m(stderr, __FILE__, __LINE__, actual, expected, NULL)
+	echeck_long_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 /* check str */
-int echeck_str_m(FILE *err, const char *file, int line, const char *actual,
-		 const char *expected, const char *msg);
+int echeck_str_m(FILE *err, const char *func, const char *file, int line,
+		 const char *actual, const char *expected, const char *msg);
 
 #define fcheck_str_m(log, actual, expected, msg)\
-	echeck_str_m(log, __FILE__, __LINE__, actual, expected, msg)
+	echeck_str_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define check_str_m(actual, expected, msg)\
-	echeck_str_m(stderr, __FILE__, __LINE__, actual, expected, msg)
+	echeck_str_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define fcheck_str(log, actual, expected)\
-	echeck_str_m(log, __FILE__, __LINE__, actual, expected, NULL)
+	echeck_str_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 #define check_str(actual, expected)\
-	echeck_str_m(stderr, __FILE__, __LINE__, actual, expected, NULL)
+	echeck_str_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 /* check ptr */
-int echeck_ptr_m(FILE *err, const char *file, int line, const void *actual,
-		 const void *expected, const char *msg);
+int echeck_ptr_m(FILE *err, const char *func, const char *file, int line,
+		 const void *actual, const void *expected, const char *msg);
 
 #define fcheck_ptr_m(log, actual, expected, msg)\
-	echeck_ptr_m(log, __FILE__, __LINE__, actual, expected, msg)
+	echeck_ptr_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define check_ptr_m(actual, expected, msg)\
-	echeck_ptr_m(stderr, __FILE__, __LINE__, actual, expected, msg)
+	echeck_ptr_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define fcheck_ptr(log, actual, expected)\
-	echeck_ptr_m(log, __FILE__, __LINE__, actual, expected, NULL)
+	echeck_ptr_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 #define check_ptr(actual, expected)\
-	echeck_ptr_m(stderr, __FILE__, __LINE__, actual, expected, NULL)
+	echeck_ptr_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 /* check unsigned int */
-int echeck_unsigned_int_m(FILE *err, const char *file, int line,
-			  unsigned int actual, unsigned int expected,
+int echeck_unsigned_int_m(FILE *err, const char *func, const char *file,
+			  int line, unsigned int actual, unsigned int expected,
 			  const char *msg);
 
 #define fcheck_unsigned_int_m(log, actual, expected, msg)\
-	echeck_unsigned_int_m(log, __FILE__, __LINE__, actual, expected, msg)
+	echeck_unsigned_int_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define check_unsigned_int_m(actual, expected, msg)\
-	echeck_unsigned_int_m(stderr, __FILE__, __LINE__, actual, expected,\
-	msg)
+	echeck_unsigned_int_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define fcheck_unsigned_int(log, actual, expected)\
-	echeck_unsigned_int_m(log, __FILE__, __LINE__, actual, expected, NULL)
+	echeck_unsigned_int_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 #define check_unsigned_int(actual, expected)\
-	echeck_unsigned_int_m(stderr, __FILE__, __LINE__, actual, expected,\
-	NULL)
+	echeck_unsigned_int_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 /* check unsigned long */
-int echeck_unsigned_long_m(FILE *err, const char *file, int line,
-			   unsigned long actual, unsigned long expected,
-			   const char *msg);
+int echeck_unsigned_long_m(FILE *err, const char *func, const char *file,
+			   int line, unsigned long actual,
+			   unsigned long expected, const char *msg);
 
 #define fcheck_unsigned_long_m(log, actual, expected, msg)\
-	echeck_unsigned_long_m(log, __FILE__, __LINE__, actual, expected, msg)
+	echeck_unsigned_long_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define check_unsigned_long_m(actual, expected, msg)\
-	echeck_unsigned_long_m(stderr, __FILE__, __LINE__, actual, expected,\
-	msg)
+	echeck_unsigned_long_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define fcheck_unsigned_long(log, actual, expected)\
-	echeck_unsigned_long_m(log, __FILE__, __LINE__, actual, expected,\
-	NULL)
+	echeck_unsigned_long_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 #define check_unsigned_long(actual, expected)\
-	echeck_unsigned_long_m(stderr, __FILE__, __LINE__, actual, expected,\
-	 NULL)
+	echeck_unsigned_long_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 /* check size_t */
-int echeck_size_t_m(FILE *err, const char *file, int line, size_t actual,
-		    size_t expected, const char *msg);
+int echeck_size_t_m(FILE *err, const char *func, const char *file, int line,
+		    size_t actual, size_t expected, const char *msg);
 
 #define fcheck_size_t_m(log, actual, expected, msg)\
-	echeck_size_t_m(log, __FILE__, __LINE__, actual, expected, msg)
+	echeck_size_t_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define check_size_t_m(actual, expected, msg)\
-	echeck_size_t_m(stderr, __FILE__, __LINE__, actual, expected, msg)
+	echeck_size_t_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, msg)
 
 #define fcheck_size_t(log, actual, expected)\
-	echeck_size_t_m(log, __FILE__, __LINE__, actual, expected, NULL)
+	echeck_size_t_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 #define check_size_t(actual, expected)\
-	echeck_size_t_m(stderr, __FILE__, __LINE__, actual, expected, NULL)
+	echeck_size_t_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, expected, NULL)
 
 /* check byte_array */
-int echeck_byte_array_m(FILE *err, const char *file, int line,
+int echeck_byte_array_m(FILE *err, const char *func, const char *file, int line,
 			unsigned char *actual, size_t actual_len,
 			unsigned char *expected, size_t expected_len,
 			const char *msg);
 
 #define fcheck_byte_array_m(log, actual, actual_len, expected, expected_len,\
 	msg)\
-	echeck_byte_array_m(log, __FILE__, __LINE__, actual, actual_len,\
-	expected, expected_len, msg)
+	echeck_byte_array_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, actual_len, expected, expected_len, msg)
 
 #define check_byte_array_m(actual, actual_len, expected, expected_len, msg)\
-	echeck_byte_array_m(stderr, __FILE__, __LINE__, actual, actual_len,\
-	expected, expected_len, msg)
+	echeck_byte_array_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, actual_len, expected, expected_len, msg)
 
 #define fcheck_byte_array(log, actual, actual_len, expected, expected_len)\
-	echeck_byte_array_m(log, __FILE__, __LINE__, actual, actual_len,\
-	expected, expected_len, NULL)
+	echeck_byte_array_m(log, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, actual_len, expected, expected_len, NULL)
 
 #define check_byte_array(actual, actual_len, expected, expected_len)\
-	echeck_byte_array_m(stderr, __FILE__, __LINE__, actual, actual_len,\
-	expected, expected_len, NULL)
+	echeck_byte_array_m(stderr, ECHECK_FUNC, __FILE__, __LINE__,\
+		 actual, actual_len, expected, expected_len, NULL)
 
 #endif /* ECHECK_H */
